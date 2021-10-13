923b07d027b64641c3523db668b42fdc9b19c9ee
AdelaBura <buraadela599@gmail.com>
2021-10-13 12:43:43 +0300

    Problemele 6, 11 si 12

--- problemele 11 si 12 .py
+++ problemele 11 si 12 .py
@@ -20,7 +20,7 @@ def get_perfect_squares(start: int, end: int):
 def test_get_perfect_squares():
     assert get_perfect_squares(3, 47) == [4, 9, 16, 25, 36]
     assert get_perfect_squares(63, 100) == [64, 81, 100]
-    assert get_perfect_squares(55, 57) == []
+    assert get_perfect_squares(2001, 2002) == []
 
 def get_leap_years(start: int, end: int):
     '''
@@ -37,26 +37,61 @@ def get_leap_years(start: int, end: int):
     for an in range(start, end + 1):
         if an % 4 == 0 and an % 100 != 0:
             lista.append(an)
-            if an % 400 == 0:
+        if an % 400 == 0:
                 lista.append(an)
     return lista
 
 def test_get_leap_years():
     assert get_leap_years(1582, 1600) == [1584, 1588, 1592, 1596, 1600]
-    assert get_leap_years(2001, 2020) == [2004, 2008, 2012, 2014, 2016, 2020]
+    assert get_leap_years(2001, 2020) == [2004, 2008, 2012, 2016, 2020]
     assert get_leap_years(2101, 2117) == [2104, 2108, 2112, 2116]
+
+def is_prime(n):
+    """
+    Afla daca un numar este prim
+    :param n: n nr natural
+    :return: Returneaza True daca n este prim, altfel False
+    """
+    if n < 2:
+        return False
+    i = 2
+    while i*i <= n:
+        if n % i == 0:
+            return False
+        i = i + 1
+    return True
+
+def is_superprime(n):
+    """
+    Afla daca un numar este superprim
+    :param n: n nr natural
+    :return: Returneaza True daca n este superprim, altfel False
+    """
+    while n > 0:
+        if not is_prime(n):
+            return False
+        n = n // 10
+    return True
+
+def test_is_superprime():
+    assert is_superprime(317) == True
+    assert is_superprime(214) == False
+
 
 def main():
     test_get_perfect_squares()
+    test_get_leap_years()
+    test_is_superprime()
     print ('''
     1. Afiseaza toate patratele perfecte dintr-un interval inchis
     2. Afiseaza anii bisecti intre doi ani dati, inclusiv anii dati
+    3. Afiseaza daca un numar este sau nu superprim
     x. Iesire
     ''')
-    cmd = input("Comanda:")
+    cmd = input("Comanda: ")
     while cmd != 'x':
             if cmd == '1':
-                interval = input('Introduceti marginile intervalului separate prin spatiu:').split(' ')
+                interval = input('Introduceti marginile intervalului separate prin spatiu: ').split(' ')
                 lim_inf = int(interval[0])
                 lim_sup = int(interval[1])
                 lista_pp = get_perfect_squares(lim_inf, lim_sup)
@@ -69,7 +104,7 @@ def main():
                 else:
                     print (f'Nu exista patrate perfecte in intervalul [{lim_inf} ,{lim_sup}]')
             elif cmd == '2':
-                ani = input('Introduceti anii separati prin spatiu:').split(' ')
+                ani = input('Introduceti anii separati prin spatiu: ').split(' ')
                 an_inceput = int(ani[0])
                 an_final = int(ani[1])
                 lista_ani = get_leap_years(an_inceput, an_final)
@@ -81,7 +116,14 @@ def main():
                     print(lista_ani_string)
                 else:
                     print(f'Nu exista ani bisecti intre anii {an_inceput} si {an_final}')
-            cmd = input('Selectati o alta comanda:')
+            elif cmd == '3':
+                n = int(input("Introduceti un numar natural: "))
+                if is_superprime(n):
+                    print(f'{n} este superprim')
+                else:
+                    print(f'{n} nu este superprim')
+
+            cmd = input('Selectati o alta comanda: ')
 
 if __name__ == '__main__':
     main()
